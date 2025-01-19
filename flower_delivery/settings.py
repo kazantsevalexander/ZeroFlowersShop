ac1dab0f0997119d04d9eff60bd9a46bce7cac79
kazantsevalexander <kazantsevalexander@gmail.com>
2025-01-19 20:32:24 +0300

    des

--- flower_delivery/settings.py
+++ flower_delivery/settings.py
@@ -61,7 +61,13 @@ LOGOUT_REDIRECT_URL = '/'
 ROOT_URLCONF = 'flower_delivery.urls'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Пути для поиска статических файлов (если используются вне приложений)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 TEMPLATES = [
     {
